00da7dd947a8636f10c666fa96ecec85ca30e175
Wilsonabdiel <wilsonabdiel86@gmail.com>
2024-02-21 12:13:14 +0100

    Readme

--- validator/app/email_validator.py
+++ validator/app/email_validator.py
@@ -5,23 +5,14 @@ import click
 def cli():
     pass
 
-
-
-@cli.command()
-@click.argument('email')
-def validate_email(email):
-    regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-    if re.match(regex, email):
-        return True
-    else:
-        return False
-
-
-
 @cli.command()
 @click.argument('email')
 def validate(email):
-    if validate_email(email):
+    regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    if re.match(regex, email):
         click.echo(f'{email} is a valid email address.')
     else:
         click.echo(f'{email} is not a valid email address.')
+
+if __name__ == "__main__":
+    cli()
